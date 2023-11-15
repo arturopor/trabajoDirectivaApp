4a50bb5d1678729c40b560adb7016ae3f5c417eb
arturopor <arturoportable@gmail.com>
2023-11-15 23:47:59 +0100

    Fin vídeo 14, directivas (I)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'trabajoDirectivaApp';
+  titulo = 'Registro de usuarios';
+  mensaje = '';
+  registrado = false;
+  nombre: string="";
+  apellidos: string="";
+
+  registrarUsuario() {
+    this.registrado = true
+
+    this.mensaje="Usuario registrado en el sistema"
+  }
 }
